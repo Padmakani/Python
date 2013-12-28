e574609fcc31ae24fccb861fbc9e79b5fedd7692
Padmakani <padmakanibe@gmail.com>
2013-12-28 23:21:48 +0530

    python

---
+++ sum.py
@@ -0,0 +1,9 @@
+
+def sum( arg1, arg2, arg3 , arg4):
+   # Add both the parameters and return them."
+   total = (arg1 + arg2 + arg3 + arg4) 
+   print "sum of the  numbers : ", total
+   return total;
+
+# Now you can call sum function
+total = sum( 2, 4, 6, 7 );
