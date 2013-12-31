fb0af68a07fe958205b05b15020d4b31e8aca5a7
Padmakani <padmakanibe@gmail.com>
2013-12-31 19:37:43 +0530

    list.py

---
+++ list.py
@@ -0,0 +1,7 @@
+n = input("Enter the list range:")
+l = [input("Enter the list elements:") for _ in range(n)]
+print "The list is:",1
+sum = 0
+for i in range(0,n):
+sum = sum+l[i]
+print "sum:",sum
